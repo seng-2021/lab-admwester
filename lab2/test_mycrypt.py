645ca56b07ed5f9f7da7e7049dd54cc0086ddcd4
Axel Wester <wester.axel@gmail.com>
2022-03-12 13:07:35 +0200

    Solved issues #1-4

--- lab2/test_mycrypt.py
+++ lab2/test_mycrypt.py
@@ -42,7 +42,7 @@ def test_invalid_char(invalid_input):
         mycrypt.encode(invalid_input)
 
 
-@pytest.mark.parametrize("invalid_input", [])
+@pytest.mark.parametrize("invalid_input", [1,-1,0,1.0, None, False, True, ["a"], bytes(5)])
 def test_invalid_types(invalid_input):
     '''Invalid parameter types should raise TypeError'''
     with pytest.raises(TypeError):
